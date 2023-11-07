636f86152974225ac819f78bd04732822bb4fd64
Kartikey Shukla <kartikey8604@gmail.com>
2023-11-08 00:03:48 +0530

    added inital routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 const App = () => {
   return (
     <Router>
       <div className="font-primary">
-        <div className="">tailwind</div>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+        {/* Nav bar */}
       </div>
     </Router>
   );
