af5ad2644e0ddbdd3daee60bd3d6392d06890619
Kartikey Shukla <kartikey8604@gmail.com>
2023-11-13 11:51:26 +0530

    added forgot password component and implemented firebase to send reset link

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,48 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../firebase.config';
+import { RiMailFill, RiArrowRightSLine } from 'react-icons/ri';
+import { toast } from 'react-toastify';
+import InputField from '../components/inputField/InputField';
+
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  const [email, setEmail] = useState('');
+
+  const onClick = async () => {
+    try {
+      await sendPasswordResetEmail(auth, email);
+      toast.success('Password reset link sent');
+    } catch (error) {
+      toast.error('Invalid email');
+    }
+  };
+
+  return (
+    <div className="py-10 px-4 max-w-7xl mx-auto">
+      <h1 className="font-bold text-3xl mb-10">Forgot Password</h1>
+      <main>
+        <InputField
+          input="Email"
+          icon={RiMailFill}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <div className="text-end text-accent font-bold my-8">
+          <Link to="/sign-in">Sign In instead</Link>
+        </div>
+        <div className="text-2xl font-bold flex items-center justify-between">
+          <span>Send Reset Link</span>
+          <button
+            onClick={onClick}
+            className="bg-accent h-12 w-12 flex justify-center items-center text-white font-extrabold rounded-full"
+          >
+            <RiArrowRightSLine size="1.8rem" />
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default ForgotPassword;
