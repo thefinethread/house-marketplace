02ef46412b3ac1700802422400803124040c9283
Kartikey Shukla <kartikey8604@gmail.com>
2023-11-25 22:49:37 +0530

    added replaced custom css active class with Navlink inline isActive callback

--- src/components/navbar/NavItem.jsx
+++ src/components/navbar/NavItem.jsx
@@ -2,7 +2,10 @@ import { NavLink } from 'react-router-dom';
 
 const NavItem = ({ to, icon: Icon, text }) => {
   return (
-    <NavLink to={to}>
+    <NavLink
+      to={to}
+      className={({ isActive }) => (isActive ? 'text-black font-bold' : '')}
+    >
       <li className="flex flex-col items-center justify-center gap-[2px] hover:cursor-pointer">
         <Icon size="1.9rem" />
         <p className="text-sm">{text}</p>
